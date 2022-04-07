02181edd9a6ee059c1eaeed273c3889467501e55
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-04-07 19:25:22 +0300

    Removed src & public

--- src/Core/Auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace PRS\Core;
-
-class Auth {
-
-   
-}
--- src/Views/home.view.php
+++
@@ -1,6 +0,0 @@
-<?php include_once 'sections/base.view.php'; ?>
-<main class="">
-    <?= "I'm home :)"; ?>
-</main>
-
-<?php include_once 'sections/footer.view.php'; ?>
