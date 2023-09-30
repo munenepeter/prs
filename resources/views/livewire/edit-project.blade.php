21c993b76daf3ab7e9dfb77693b965721c7cb3ce
munenepeter <peternjega7@gmail.com>
2023-09-30 13:22:00 +0300

    better display to tasks target

--- resources/views/livewire/edit-project.blade.php
+++ resources/views/livewire/edit-project.blade.php
@@ -1,225 +1,137 @@
 <div>
-	<h1 class="uk-text-bold uk-text-capitalize">Edit {{ $project->name }}</h1>
-	<hr class="uk-divider-small">
+    <h1 class="uk-text-bold uk-text-capitalize">Edit {{ $project->name }}</h1>
+    <hr class="uk-divider-small">
 
-	<form wire:submit.prevent="edit"
-		  class="uk-grid uk-grid-medium uk-child-width-1-1 uk-form-horizontal"
-		  uk-grid
-	>
-		<div>
-			<x-label for="project_name"
-					 value="Define the project name"
-			/>
-			<x-input type="text"
-					 id="project_name"
-					 wire:model.defer="project.name"
-					 placeholder="Project name"
-					 :hasError="$errors->has('project.name')"
-			/>
-			<x-form-error input="project.name" />
-		</div>
-		<div class="uk-grid-margin uk-first-column">
-			<x-label for="project_status"
-					 value="Define the project status"
-			/>
+    <form wire:submit.prevent="edit" class="uk-grid uk-grid-medium uk-child-width-1-1 uk-form-horizontal" uk-grid>
+        <div>
+            <x-label for="project_name" value="Define the project name" />
+            <x-input type="text" id="project_name" wire:model.defer="project.name" placeholder="Project name"
+                :hasError="$errors->has('project.name')" />
+            <x-form-error input="project.name" />
+        </div>
+        <div class="uk-grid-margin uk-first-column">
+            <x-label for="project_status" value="Define the project status" />
 
-			<x-select wire:model.defer="project.status"
-					  id="project_status"
-					  choose_option="Choose the project status"
-					  :hasError="$errors->has('project.status')"
-					  :collection="$this->projectStatuses"
+            <x-select wire:model.defer="project.status" id="project_status" choose_option="Choose the project status"
+                :hasError="$errors->has('project.status')" :collection="$this->projectStatuses" />
+            <x-form-error input="project.status" />
+        </div>
+        <div class="uk-grid-margin uk-first-column">
+            <x-label for="project_description" value="Tell us a bit about the project (optional)" />
+            <x-textarea wire:model.defer="project.description" id="project_description"
+                placeholder="Provide some brief information on the project." :hasError="$errors->has('project.description')" />
+            <x-form-error input="project.description" />
+        </div>
+        @if (!$create_new_client)
+            <div class="uk-grid-margin uk-first-column">
+                <x-label for="client" value="Assign the client who owns the project" />
+                <x-select wire:model.defer="project.client.id" id="client" choose_option="Choose a client"
+                    :hasError="$errors->has('project.client.id')" :collection="$this->clients" />
+                <x-form-error input="project.client.id" />
+            </div>
+        @endif
+        <div class="uk-grid-margin uk-first-column">
+            <div class="uk-grid uk-grid-small uk-grid-stack" uk-grid>
+                <div class="uk-width-auto uk-first-column">
+                    <label class="switch" for="create_new_client">
+                        <input wire:model.lazy="create_new_client" type="checkbox" id="create_new_client">
+                        <div class="switch-slider switch-on-off switch-big"></div>
+                    </label>
+                </div>
+                <div class="uk-width-expand">
+                    <small class="uk-text-small">Create new client</small>
+                </div>
+            </div>
+        </div>
 
-			/>
-			<x-form-error input="project.status" />
-		</div>
-		<div class="uk-grid-margin uk-first-column">
-			<x-label for="project_description"
-					 value="Tell us a bit about the project (optional)"
-			/>
-			<x-textarea wire:model.defer="project.description"
-						id="project_description"
-						placeholder="Provide some brief information on the project."
-						:hasError="$errors->has('project.description')"
-			/>
-			<x-form-error input="project.description" />
-		</div>
-		@if (!$create_new_client)
-			<div class="uk-grid-margin uk-first-column">
-				<x-label for="client"
-						 value="Assign the client who owns the project"
-				/>
-				<x-select wire:model.defer="project.client.id"
-						  id="client"
-						  choose_option="Choose a client"
-						  :hasError="$errors->has('project.client.id')"
-						  :collection="$this->clients"
+        @if ($create_new_client)
+            <div class="uk-grid-margin uk-first-column">
+                <x-label for="new_client" value="Client name" />
+                <x-input type="text" id="new_client" wire:model.defer="new_client"
+                    placeholder="Input the client name" :hasError="$errors->has('new_client')" />
+                <x-form-error input="new_client" />
+            </div>
+        @endif
+        <div class="uk-grid-margin uk-first-column">
+            <x-label for="project_manager" value="Assign a project manager" />
+            <x-select wire:model.defer="project.manager.id" id="project_manager"
+                choose_option="Choose a project manager" :hasError="$errors->has('project.manager.id')" :collection="$this->projectManagers" />
+            <x-form-error input="project.manager.id" />
+        </div>
 
-				/>
-				<x-form-error input="project.client.id" />
-			</div>
-		@endif
-		<div class="uk-grid-margin uk-first-column">
-			<div
-				class="uk-grid uk-grid-small uk-grid-stack"
-				uk-grid
-			>
-				<div class="uk-width-auto uk-first-column">
-					<label
-						class="switch"
-						for="create_new_client"
-					>
-						<input
-							wire:model.lazy="create_new_client"
-							type="checkbox"
-							id="create_new_client"
-						>
-						<div class="switch-slider switch-on-off switch-big"></div>
-					</label>
-				</div>
-				<div class="uk-width-expand">
-					<small class="uk-text-small">Create new client</small>
-				</div>
-			</div>
-		</div>
+        @if ($project->tasks->isNotEmpty())
+            <div class="uk-grid-margin uk-tile uk-tile-small uk-padding-small-horizontal">
+                <h2 class="uk-h4">Project Tasks</h2>
+                <ul class="uk-list uk-list-striped">
+                    <li class="uk-flex uk-flex-middle uk-flex-between">
+                        <span># &nbsp;Task Name</span>
+                        <span>Task Target</span>
+                        <span>Remove Task</span>
+                    </li>
+                    @foreach ($project->tasks as $task)
+                        <li class="uk-flex uk-flex-middle uk-flex-between">
+                            <span> {{ $loop->iteration }}. &nbsp; {{ $task->name }}</span>
+                            <span>{{ $task->target }}</span>
+                            <button type="button" wire:click="removeTask({{ $task->id }})"
+                                class="remove-employee uk-icon uk-close uk-svg uk-margin-small-left"
+                                uk-close="ratio: 0.8"></button>
+                        </li>
+                    @endforeach
+                </ul>
 
-		@if ($create_new_client)
-			<div class="uk-grid-margin uk-first-column">
-				<x-label for="new_client"
-						 value="Client name"
-				/>
-				<x-input type="text"
-						 id="new_client"
-						 wire:model.defer="new_client"
-						 placeholder="Input the client name"
-						 :hasError="$errors->has('new_client')"
-				/>
-				<x-form-error input="new_client" />
-			</div>
-		@endif
-		<div class="uk-grid-margin uk-first-column">
-			<x-label for="project_manager"
-					 value="Assign a project manager"
-			/>
-			<x-select wire:model.defer="project.manager.id"
-					  id="project_manager"
-					  choose_option="Choose a project manager"
-					  :hasError="$errors->has('project.manager.id')"
-					  :collection="$this->projectManagers"
-			/>
-			<x-form-error input="project.manager.id" />
-		</div>
+            </div>
 
-		@if ($project->tasks->isNotEmpty())
-			<div class="uk-grid-margin uk-tile uk-tile-small uk-padding-small-horizontal">
-				<h2 class="uk-h4">Project Tasks</h2>
-				<ul class="uk-list uk-list-striped">
+        @endif
 
-					@foreach($project->tasks as $task)
-						<li class="uk-flex uk-flex-middle uk-flex-between">
-							<span>	{{$loop->iteration}}. 	{{ $task->name }}</span>
-							<button
-								type="button"
-								wire:click="removeTask({{ $task->id }})"
-								class="remove-employee uk-icon uk-close uk-svg uk-margin-small-left"
-								uk-close="ratio: 0.8"
-							></button>
-						</li>
-					@endforeach
-				</ul>
+        <div class="uk-grid-margin uk-first-column">
+            <div class="uk-grid uk-grid-small uk-grid-stack" uk-grid>
+                <div class="uk-width-auto uk-first-column">
+                    <label class="switch" for="add_new_tasks">
+                        <input wire:model.lazy="create_tasks" type="checkbox" id="add_new_tasks">
+                        <div class="switch-slider switch-on-off switch-big"></div>
+                    </label>
+                </div>
+                <div class="uk-width-expand">
+                    <small class="uk-text-small">Assign tasks along with the project</small>
+                </div>
+            </div>
+        </div>
+        @if ($create_tasks)
+            <div class="uk-grid-margin">
+                <p class="uk-text-lead">Add a new task to {{ $project->name }} project</p>
 
-			</div>
+                <div>
+                    <x-label for="project_manager" value="Task Name" />
+                    <div class="uk-form-controls uk-grid uk-flex-middle uk-grid-small" uk-grid>
+                        <div class="uk-width-expand">
+                            <input type="text" wire:model.defer="task" id="task" @class(['uk-input', 'uk-form-danger' => $errors->has('task')])
+                                placeholder="Name of task">
+                        </div>
+                        <x-label for="project_manager" value="Task Target" />
+                        <div class="uk-width-expand">
+                            <input type="number" wire:model.defer="target" id="target" @class(['uk-input', 'uk-form-danger' => $errors->has('target')])
+                                placeholder="Target of the task">
+                        </div>
+                        <div class="uk-width-auto">
+                            <button type="button" wire:click="addTask" class="uk-button uk-button-primary">Add Task
+                            </button>
+                        </div>
+                        <div class="uk-width-1-1">
+                            <x-form-error input="task" />
+                        </div>
+                    </div>
+                </div>
 
-		@endif
+            </div>
+        @endif
 
-		<div class="uk-grid-margin uk-first-column">
-			<div
-				class="uk-grid uk-grid-small uk-grid-stack"
-				uk-grid
-			>
-				<div class="uk-width-auto uk-first-column">
-					<label
-						class="switch"
-						for="add_new_tasks"
-					>
-						<input
-							wire:model.lazy="create_tasks"
-							type="checkbox"
-							id="add_new_tasks"
-						>
-						<div class="switch-slider switch-on-off switch-big"></div>
-					</label>
-				</div>
-				<div class="uk-width-expand">
-					<small class="uk-text-small">Assign tasks along with the project</small>
-				</div>
-			</div>
-		</div>
-		@if($create_tasks)
-			<div class="uk-grid-margin">
-				<p class="uk-text-lead">Add a new task to {{ $project->name }} project</p>
+        <div class="uk-grid-margin uk-first-column">
+            <button type="submit" class="uk-button uk-button-primary uk-width-1-1">
+                Save Project
+            </button>
+        </div>
+    </form>
 
-				<div>
-					<x-label for="project_manager"
-							 value="Task Name"
-					/>
-					<div
-						class="uk-form-controls uk-grid uk-flex-middle uk-grid-small"
-						uk-grid
-					>
-						<div class="uk-width-expand">
-							<input
-								type="text"
-								wire:model.defer="task"
-								id="task"
-								@class([
-									   'uk-input',
-									   'uk-form-danger' => $errors->has('task')
-									])
-								placeholder="Name of task"
-							>
-						</div>
-						<x-label for="project_manager"
-							 value="Task Target"
-					/>
-						<div class="uk-width-expand">
-							<input
-								type="number"
-								wire:model.defer="target"
-								id="target"
-								@class([
-									   'uk-input',
-									   'uk-form-danger' => $errors->has('target')
-									])
-								placeholder="Target of the task"
-							>
-						</div>
-						<div class="uk-width-auto">
-							<button
-								type="button"
-								wire:click="addTask"
-								class="uk-button uk-button-primary"
-							>Add Task
-							</button>
-						</div>
-						<div class="uk-width-1-1">
-							<x-form-error input="task" />
-						</div>
-					</div>
-				</div>
-
-			</div>
-
-		@endif
-
-		<div class="uk-grid-margin uk-first-column">
-			<button type="submit"
-					class="uk-button uk-button-primary uk-width-1-1"
-			>
-				Edit Project
-			</button>
-		</div>
-	</form>
-
-	<x-project-insert-scripts />
+    <x-project-insert-scripts />
 
 </div>
--- resources/views/livewire/view-project-tasks.blade.php
+++ resources/views/livewire/view-project-tasks.blade.php
@@ -1,83 +1,82 @@
 <div class="uk-width-1-1">
-	<h1 class="uk-heading-small uk-text-bold uk-text-capitalize">
-		{{ $project->name }} Tasks
-	</h1>
-	<hr class="uk-divider-small">
+    <h1 class="uk-heading-small uk-text-bold uk-text-capitalize">
+        {{ $project->name }} Tasks
+    </h1>
+    <hr class="uk-divider-small">
 
-	<div class="uk-flex uk-flex-between uk-flex-middle">
-		<h2>All Tasks</h2>
-		<a href="{{ route('projects.index') }}"
-		   class="uk-button-primary uk-button">All Projects</a>
-	</div>
-	<div class="uk-overflow-auto">
-		<table class="uk-table uk-table-hover uk-table-middle uk-table-divider">
-			<thead>
-				<tr>
-					<th class="uk-table-shrink">#</th>
-					<th class="uk-table-expand">Name</th>
-					<th class="uk-table-expand">Target</th>
-					<th class="uk-width-small">Unit type</th>
-					<th>Actions</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($project->tasks as $task)
-					<tr>
-						<td>{{ $loop->iteration }}</td>
-						<td>{{ $task->name }}</td>
-						<td>{{ $task->target }}</td>
-						<td>{{ $task->unit_type->name }}</td>
-						<td>
-							<div class="uk-button-group">
-								<button uk-toggle="target: #edit-task-{{ $task->id }}"
-										type="button"
-										class="uk-button uk-button-secondary uk-margin-small-right"
-								>Edit
-								</button>
-								<livewire:edit-task-modal key="{{ $task->id . ' ' . $task->name }}"
-														  :task_id="$task->id"
-								/>
+    <div class="uk-flex uk-flex-between uk-flex-middle">
+        <h2>All Tasks</h2>
+        <a href="{{ route('projects.index') }}" class="uk-button-primary uk-button">All Projects</a>
+    </div>
+    <div class="uk-overflow-auto">
+        <table class="uk-table uk-table-hover uk-table-middle uk-table-divider">
+            <thead>
+                <tr>
+                    <th class="uk-table-shrink">#</th>
+                    <th class="uk-table-expand">Name</th>
+                    <th class="uk-table-expand">Target</th>
+                    <th class="uk-width-small">Unit type</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($project->tasks as $task)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $task->name }}</td>
+                        <td>
+                            {{ $task->target }}
+                            {{ ['HOUR' => 'units', 'CHARACTERS' => 'characters', 'PAGES' => 'pages'][$task->unit_type->name] }}
+                            per hour
 
-								<button type="button" onclick="confirm('Are you sure you want to delete this project task?') || event.stopImmediatePropagation()"
-										wire:click="deleteTask({{ $task->id }})"
-										class="uk-button uk-button-danger"
-								>
-									Delete
-								</button>
-							</div>
+                        </td>
+                        <td>{{ $task->unit_type->name }}</td>
+                        <td>
+                            <div class="uk-button-group">
+                                <button uk-toggle="target: #edit-task-{{ $task->id }}" type="button"
+                                    class="uk-button uk-button-secondary uk-margin-small-right">Edit
+                                </button>
+                                <livewire:edit-task-modal key="{{ $task->id . ' ' . $task->name }}" :task_id="$task->id" />
 
-						</td>
-					</tr>
-				@endforeach
-			</tbody>
-		</table>
+                                <button type="button"
+                                    onclick="confirm('Are you sure you want to delete this project task?') || event.stopImmediatePropagation()"
+                                    wire:click="deleteTask({{ $task->id }})" class="uk-button uk-button-danger">
+                                    Delete
+                                </button>
+                            </div>
 
-	</div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-	@push('scripts')
-		<script>
-			const closeModal = elem => UIkit.modal(elem).hide();
+    </div>
 
-			window.addEventListener('failed-to-edit-task', event => {
-				closeModal(event.detail.modal);
+    @push('scripts')
+        <script>
+            const closeModal = elem => UIkit.modal(elem).hide();
 
-				dangerNotification(event.detail.message);
-			});
+            window.addEventListener('failed-to-edit-task', event => {
+                closeModal(event.detail.modal);
 
-			window.addEventListener('edit-task-successfully', event => {
-				closeModal(event.detail.modal);
+                dangerNotification(event.detail.message);
+            });
 
-				successNotification(event.detail.message);
-			});
+            window.addEventListener('edit-task-successfully', event => {
+                closeModal(event.detail.modal);
 
-			window.addEventListener('task-deleted', event => {
+                successNotification(event.detail.message);
+            });
 
-				successNotification(event.detail.message);
-			});
+            window.addEventListener('task-deleted', event => {
 
-			window.addEventListener('no-change', event => {
-				closeModal(event.detail.modal);
-			});
-		</script>
-	@endpush
+                successNotification(event.detail.message);
+            });
+
+            window.addEventListener('no-change', event => {
+                closeModal(event.detail.modal);
+            });
+        </script>
+    @endpush
 </div>
