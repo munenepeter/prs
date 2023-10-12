a9a3e13a60e5ffaa56b1adbda47b25f454f227db
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-10-12 07:13:10 +0000

    a little reformat

--- resources/views/livewire/view-daily-reports.blade.php
+++ resources/views/livewire/view-daily-reports.blade.php
@@ -205,12 +205,13 @@
                     <td style="font-weight: 800" colspan="3">Total Duration: {{ $totalDuration }}
                     </td>
                     <td style="font-weight: 800" colspan="2">Total Units/hr: {{ $totalUnitshr }} </td>
-                  @if(auth()->user()->isAdmin() || auth()->user()->isProjectManager() )
-                      <td style="font-weight: 800" colspan="2">On Target: {{ 'X' }}
-                    </td>
-                    <td style="font-weight: 800" colspan="2">Below Target: {{ 'Y' }}
-                    </td>
-                  @endif
+                    @if (auth()->user()->isAdmin() ||
+                            auth()->user()->isProjectManager())
+                        <td style="font-weight: 800" colspan="2">On Target: {{ 'X' }}
+                        </td>
+                        <td style="font-weight: 800" colspan="2">Below Target: {{ 'Y' }}
+                        </td>
+                    @endif
                 </tr>
                 <tr>
                     <td colspan="8">
