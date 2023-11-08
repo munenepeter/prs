69d8b3e0cc5a2f900e34d35226747e24f52e4b8a
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-08 13:18:29 +0300

    populate the select to use perfomances

--- resources/views/livewire/latest-daily-reports.blade.php
+++ resources/views/livewire/latest-daily-reports.blade.php
@@ -57,7 +57,7 @@
                     <div>
                     <x-label for="duration" value="Filter by perfomance e.g Above target" />
                     <x-select wire:model.defer="duration" id="duration" choose_text="Choose the perfomance"
-                    :hasError="$errors->has('duration')" :collection="$this->durations" :hasError="$errors->has('duration')" :disabled="$has_filter" />
+                    :hasError="$errors->has('duration')" :collection="$this->perfomances" :hasError="$errors->has('duration')" :disabled="$has_filter" />
                     <x-form-error input="task" />
                     </div>
                 </div>
