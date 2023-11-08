a701690a2e1e85f2806edbbd973328a30f3683ef
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-08 14:53:04 +0300

    fix #3 reduce font-size tfoot

--- resources/views/livewire/view-daily-reports.blade.php
+++ resources/views/livewire/view-daily-reports.blade.php
@@ -182,7 +182,7 @@
                 <tr style="font-weight: 800">
                     <td>Total:{{ $totalProjects <= 0 ? 'N/A' : $totalProjects }} </td>
                     <td>Total:{{ $totalTasks <= 0 ? 'N/A' : $totalTasks }}</td>
-                    <td>Total:{{ $totalDuration->forHumans(['short' => true]) }}</td>                       
+                    <td style="font-size:12px;">Total:{{ $totalDuration->forHumans(['short' => true]) }}</td>                       
                     <td>Above Target: {{ $aboveTarget ?? 'N/A' }}</td>
                     <td>Below Target: {{ $belowTarget ?? 'N/A' }}</td>
                     <td>On Target {{$onTarget}}</td>
