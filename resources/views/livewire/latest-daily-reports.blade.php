c7ecd22d704e90d4effd286142c7332da7169c56
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-08 13:55:17 +0300

    update performance to std

--- resources/views/livewire/latest-daily-reports.blade.php
+++ resources/views/livewire/latest-daily-reports.blade.php
@@ -171,11 +171,9 @@
                         </td>
                         <td> 
 
-                            <span class="" style="color: {{ $report->perfomance['color'] }}">
-                                {{ $report->perfomance['status'] }}
-                            </span><br>
-                            {{$report->target}}
-                            <br>
+                            <span class="" style="color: {{ $report->perfomanceColor }}">
+                                {{ $report->perfomance }}
+                            </span>
                             <span class="uk-text-small">Target: {{ $report->task->target }}
                                 <span> {{ $report->formattedTarget }} </span></span>
                         </td>
