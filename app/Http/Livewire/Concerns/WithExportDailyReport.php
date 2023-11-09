7644326a9deb79164e7f2be618ae4e361a5f496a
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-09 11:37:38 +0300

    Fix: download the report as usual

--- app/Http/Livewire/Concerns/WithExportDailyReport.php
+++ app/Http/Livewire/Concerns/WithExportDailyReport.php
@@ -32,9 +32,7 @@ trait WithExportDailyReport {
 
         $reports = $reports->get();
 
-        return view('daily_report_pdf', compact('reports'));
 
-        /*
         $pdfContent = view('daily_report_pdf', compact('reports'))->render();
 
         // Generate PDF
@@ -48,7 +46,7 @@ trait WithExportDailyReport {
 
         // Provide a download link to the saved PDF
         return response()->download(public_path($this->getExportFileName() . '.pdf'))->deleteFileAfterSend();
-*/
+
     }
 
     public function getExportFileName() {
