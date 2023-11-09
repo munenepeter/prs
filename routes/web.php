7a33aeb4dc2e8ddd694218cdfbfc8e529a07086b
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-09 12:57:52 +0300

    clean-up: remove unused imports & comments

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\FirstTimeLoginController;
-use Barryvdh\DomPDF\Facade\Pdf as PDF;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +16,7 @@ use Barryvdh\DomPDF\Facade\Pdf as PDF;
 |
 */
 Route::permanentRedirect('/', 'login')->name('homepage');
-//Route::prefix('projects')->name('projects.')->group(function () {
-//    Route::get('/', \App\Http\Livewire\ViewProjects::class)->name('index');
-//    Route::get('/create', \App\Http\Livewire\CreateProject::class)->name('create');
-//    Route::get('/{project}', \App\Http\Livewire\ShowProject::class)->name('show');
-//    Route::get('/{project}/edit', \App\Http\Livewire\EditProject::class)->name('edit');
-//});
+
 Route::prefix('login')->name('login.')->group(function () {
     Route::get('first_time', [FirstTimeLoginController::class, 'create'])->name('first.time.create');
     Route::post('first_time', [FirstTimeLoginController::class, 'store'])->name('first.time.store');
